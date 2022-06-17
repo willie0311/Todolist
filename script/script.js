9d6fc1d1abde6343b1c20c759ac1afed5ff4283f
willie <willie@gmail.com>
2022-06-17 11:11:06 +0800

    click 修正上一版 JS錯誤

--- script/script.js
+++ script/script.js
@@ -1,5 +1,15 @@
 
 let add = document.querySelector("form button");
-add.addEventListener("clock" , (e) => {
+add.addEventListener("click" , (e) => {
     e.preventDefault(); //停止表單送出
+    // console.log(e); 表單會馬上送出歸零 submit
+
+
+let form = e.target.parentElement;
+let todoText = form.children[0].value;
+let todoY = form.children[1].value;
+let todoM = form.children[2].value;
+let todoD = form.children[3].value;
+console.log(todoText);
+
 });
