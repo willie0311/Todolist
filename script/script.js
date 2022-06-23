8d4506bb5b27c09ef2b7d200866ab8c5cfa2c727
willie <willie@gmail.com>
2022-06-23 12:40:25 +0800

    讀寫local storage內部"舊"資料顯示出來

--- script/script.js
+++ script/script.js
@@ -98,7 +98,70 @@ if (myList == null) {
 //在console內看行為
 console.log(JSON.parse(localStorage.getItem("list")));
 
-
 //再把DIV 塞回去 Section
 section.appendChild(todo);
 });
+
+
+//這一段寫回去讀寫local storage內部資料顯示出來
+
+let myList = localStorage.getItem("list");
+if (myList !== null) {
+    let myListArray = JSON.parse(myList);
+    //回傳local storage需要加上item
+    myListArray.forEach( item => {
+
+        // 創立一個DIV
+        let todo = document.createElement('div');
+        todo.classList.add("todo");
+        // 創立一個P 塞text文字的值
+        let text = document.createElement('p');
+        text.classList.add("todo-text");
+        text.innerText = item.todoText //需要加上item  很重要
+        // 創立一個P 塞年月份文字的值
+        let time = document.createElement('p');
+        time.classList.add("todo-time");
+        time.innerText = item.todoY +"/" + item.todoM +"/" + item.todoD ; //需要加上item  很重要
+
+        //把這兩個值 塞回去DIV
+        todo.appendChild(text);
+        todo.appendChild(time);
+
+        //第三次KIN的位置
+        //打勾 
+        let completeButton = document.createElement("button"); //按鈕
+        completeButton.classList.add("complete");
+        completeButton.innerHTML = '<i class = "fas fa-check"></i>' //圖
+
+        //打勾按鈕觸發 執行新增Class done 
+        completeButton.addEventListener("click" , e => {
+            // console.log(e.target.parentElement)  回上層
+            let todoItem = e.target.parentElement;  ////回到上一層div(父層)
+            todoItem.classList.toggle("done");  // add 換成 toggle 切換按鈕 刪除線可以顯示於不顯示
+        });
+
+        //垃圾桶
+        let trashButton = document.createElement("button");
+        trashButton.classList.add("trash");
+        trashButton.innerHTML = '<i class = "fas fa-trash"></i>' //圖
+
+        //垃圾桶按鈕觸發 執行動畫 刪除list 把隱藏的位置直截刪除 remove
+        trashButton.addEventListener("click" , e => {
+            let todoItem= e.target.parentElement;  //回到上一層div(父層)
+            todoItem.addEventListener("animationend" , () =>{
+                todoItem.remove(); //動畫結束後刪除
+            });
+        todoItem.style.animation = "scaleDown 0.3s forwards"; //先讓動畫跑完
+        });
+
+
+        todo.appendChild(completeButton);  //add 在日期後面的打勾
+        todo.appendChild(trashButton); //add 在日期後面的垃圾桶
+
+        //list動畫顯示效果
+        todo.style.animation = "scaleUp 0.3s forwards";
+        //再把DIV 塞回去 Section
+        section.appendChild(todo);
+
+    });
+}
